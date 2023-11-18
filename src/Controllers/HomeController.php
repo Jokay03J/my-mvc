39ef8d27b7b4e07f614a0d85162ad2b7f0ca0ed3
jokay03J <magrojoan3@gmail.com>
2023-11-18 20:09:18 +0100

    update controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,14 +1,19 @@
 <?php
 class HomeController extends BaseController
 {
-    public function index()
+    public function index(Request $request, Response $response)
     {
-        $this->view->render("home");
+        $this->view->render("home", ["user" => ["id" => 1, "username" => "jokay03J"]]);
     }
 
-    public function test()
+    public function test(Request $request, Response $response)
     {
-        return new Response("test", ["status" => 500]);
+        $this->view->render("form");
+    }
+
+    public function submit(Request $request, Response $response)
+    {
+        $this->view->render("form", ["errors" => "sa marche !", "post" => $request->body, "files" => $request->files]);
     }
 }
 ?>
